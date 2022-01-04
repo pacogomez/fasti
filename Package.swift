0a27360f2134c197db643a754622abdc89a27fe2
Paco Gomez <contact@pacogomez.com>
2022-01-03 21:39:17 -0600

    table

--- Package.swift
+++ Package.swift
@@ -6,14 +6,15 @@ import PackageDescription
 let package = Package(
     name: "fasti",
     dependencies: [
-      .package(url: "https://github.com/jakeheis/SwiftCLI", from: "6.0.0")
+      .package(url: "https://github.com/jakeheis/SwiftCLI", from: "6.0.0"),
+      .package(url: "https://github.com/scottrhoyt/SwiftyTextTable.git", from: "0.5.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "fasti",
-            dependencies: ["SwiftCLI"]),
+            dependencies: ["SwiftCLI", "SwiftyTextTable"]),
         .testTarget(
             name: "fastiTests",
             dependencies: ["fasti"]),
--- Sources/fasti/main.swift
+++ Sources/fasti/main.swift
@@ -1,6 +1,14 @@
 import EventKit
 import SwiftCLI
 
+import SwiftyTextTable
+
+// First create some columns
+let eventCol = TextTableColumn(header: "Event")
+let startCol = TextTableColumn(header: "Start")
+let endCol = TextTableColumn(header: "End")
+let calendarCol = TextTableColumn(header: "Calendar")
+
 class GreetCommand: Command {
     let name = "greet"
 
@@ -23,7 +31,12 @@ let eventStore = EKEventStore()
 let calendars = eventStore.calendars(for: .event)
 
 let now = Date()
-let weekFromNow = now.addingTimeInterval(7.0 * 24.0 * 3600.0)
+let weekFromNow = now.addingTimeInterval(1 * 24.0 * 3600.0)
+
+let localDateFormatter = DateFormatter()
+localDateFormatter.dateStyle = .medium
+localDateFormatter.timeStyle = .medium
+
 // for (_, calendar) in calendars.enumerated(){
 //   print(calendar.title)
 //   let predicate = eventStore.predicateForEvents(withStart: now, end: weekFromNow, calendars: [calendar])
@@ -32,13 +45,22 @@ let weekFromNow = now.addingTimeInterval(7.0 * 24.0 * 3600.0)
 //     print("\t\(event.title!) - \(event.calendar.title!)\n\t\t\(event.startDate!)-\(event.endDate!)")
 //   }
 // }
+
+var table = TextTable(columns: [eventCol, startCol, endCol, calendarCol])
+
   let predicate = eventStore.predicateForEvents(withStart: now, end: weekFromNow, calendars: calendars)
   let matchingEvents = eventStore.events(matching: predicate)
   for event in matchingEvents{
-    print("\(event.title!) - \(event.calendar.title)\n\t\(event.startDate!)-\(event.endDate!)")
+    table.addRow(values:[event.title!, localDateFormatter.string(from:event.startDate!),localDateFormatter.string(from:event.endDate!),event.calendar.title])
+    // print("\(event.title!) - \(event.calendar.title)\n\t\(localDateFormatter.string(from:event.startDate!)) - \(localDateFormatter.string(from:event.endDate!))")
     // print(event)
   }
 
+
+  // Then render the table and use
+  let tableString = table.render()
+  print(tableString)
+
   let greeter = CLI(name: "greeter")
   greeter.commands = [GreetCommand()]
-  greeter.go()
+  // greeter.go()
