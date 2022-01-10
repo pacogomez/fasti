ac2b9b5034168894d4a8ff08c202844f3d38b20d
Paco Gomez <contact@pacogomez.com>
2022-01-10 11:09:50 -0600

    status

--- Sources/fasti/main.swift
+++ Sources/fasti/main.swift
@@ -6,7 +6,7 @@ import SwiftCLI
 import SwiftyTextTable
 
 func completion(granted: Bool, error: Error!){
-  // print("granted: \(granted)")
+    // print("granted: \(granted)")
 }
 
 let calendar = Calendar.autoupdatingCurrent
@@ -16,6 +16,8 @@ EKEventStore().requestAccess(to:.event, completion: completion)
 let eventStore = EKEventStore()
 
 let sources = eventStore.sources
+
+let statuses: [String] = ["scheduled", "confirmed", "tentative", "canceled"]
 
 class GetGroup: CommandGroup {
     let name = "get"
@@ -30,10 +32,10 @@ class AddGroup: CommandGroup {
 }
 
 class AddEventCommand: Command {
-  let name = "event"
-  func execute() throws {
-    print("not implemented")
-  }
+    let name = "event"
+    func execute() throws {
+        print("not implemented")
+    }
 }
 
 class DelGroup: CommandGroup {
@@ -43,75 +45,96 @@ class DelGroup: CommandGroup {
 }
 
 class DelEventCommand: Command {
-  let name = "event"
-  func execute() throws {
-    print("not implemented")
-  }
+    let name = "event"
+    func execute() throws {
+        print("not implemented")
+    }
 }
 
 class ListEventsCommand: Command {
     let name = "events"
-
+    
     @Key("-n", "--next")
     var next: Int?
-
+    
     @Key("-s", "--start")
     var start: String?
-
-    // @Flag("-o", "--output")
-    // var outputFormat: String
-
+    
+    @Key("-o", "--output")
+    var outputFormat: String?
+    
     func execute() throws {
-      var startDate: Date? = Date()
-      if let start = start {
-        if start.count > 0 {
-          let dateFormatter = DateFormatter()
-          dateFormatter.dateFormat = "yyyy-MM-dd"
-          startDate = dateFormatter.date(from: start)
-          if startDate == nil {
-            print("invalid start date: \(start)")
-            return
-          }
+        var startDate: Date? = Date()
+        if let start = start {
+            if start.count > 0 {
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyy-MM-dd"
+                startDate = dateFormatter.date(from: start)
+                if startDate == nil {
+                    print("invalid start date: \(start)")
+                    return
+                }
+            }
         }
-      }
-      let next = next ?? 1
-      let calendars = eventStore.calendars(for: .event)
-
-      let endOfToday = Calendar.current.date(bySettingHour:23, minute:59, second: 59, of: startDate!) ?? startDate!
-      let later = Calendar.current.date(byAdding: .day, value: next, to:endOfToday) ?? startDate!
-
-      let localDateFormatter = DateFormatter()
-      localDateFormatter.dateFormat = "yyyy-MM-dd hh:mm a EEEEE"
-
-      let eventCol = TextTableColumn(header: "Event")
-      let startCol = TextTableColumn(header: "Start")
-      let endCol = TextTableColumn(header: "End")
-      let calendarCol = TextTableColumn(header: "Calendar")
-
-      var table = TextTable(columns: [startCol, endCol, eventCol, calendarCol])
-
-      let predicate = eventStore.predicateForEvents(withStart: startDate!, end: later, calendars: calendars)
-      let matchingEvents = eventStore.events(matching: predicate)
-      for event in matchingEvents{
-        table.addRow(values:[localDateFormatter.string(from:event.startDate!),localDateFormatter.string(from:event.endDate!),event.title!, event.calendar.source.title+"/"+event.calendar.title])
-        // print(event)
-        // print(event.attendees)
-        // print("--")
-      }
-      let tableString = table.render()
-      print(tableString)
+        let next = next ?? 1
+        let calendars = eventStore.calendars(for: .event)
+        
+        let endOfToday = Calendar.current.date(bySettingHour:23, minute:59, second: 59, of: startDate!) ?? startDate!
+        let later = Calendar.current.date(byAdding: .day, value: next, to:endOfToday) ?? startDate!
+        
+        let localDateFormatter = DateFormatter()
+        localDateFormatter.dateFormat = "yyyy-MM-dd hh:mm a EEEEE"
+        
+        let predicate = eventStore.predicateForEvents(withStart: startDate!, end: later, calendars: calendars)
+        let matchingEvents = eventStore.events(matching: predicate)
+        
+        var output = "table"
+        if let outputFormat = outputFormat {
+            if outputFormat.count == 0 {
+                output = "table"
+            } else {
+                output = outputFormat
+            }
+        }
+        
+        if output == "table"{
+            let eventCol = TextTableColumn(header: "Event")
+            let startCol = TextTableColumn(header: "Start")
+            let endCol = TextTableColumn(header: "End")
+            let statusCol = TextTableColumn(header: "Status")
+            let calendarCol = TextTableColumn(header: "Calendar")
+            var table = TextTable(columns: [startCol, endCol, eventCol, statusCol, calendarCol])
+            for event in matchingEvents{
+                table.addRow(values:[localDateFormatter.string(from:event.startDate!),localDateFormatter.string(from:event.endDate!),event.title!, statuses[event.status.rawValue],
+                                     event.calendar.source.title+"/"+event.calendar.title])
+            }
+            let tableString = table.render()
+            print(tableString)
+        }else if output == "report" {
+            for event in matchingEvents{
+                print(event.title!)
+                print("  \(localDateFormatter.string(from:event.startDate!))-\(localDateFormatter.string(from:event.endDate!)), \(statuses[event.status.rawValue])")
+                if event.attendees != nil{
+                    print("  ", terminator: "")
+                    for attendee in event.attendees!{
+                        print("\(attendee.name!)", terminator: ", ")
+                    }
+                }
+                print("\n--")
+            }
+        }
     }
 }
 
 class ListCalendarsCommand: Command {
     let name = "calendars"
-
+    
     func execute() throws {
         for (_, source) in sources.enumerated(){
-           print(source.title, source.value(forKey: "displayOrder")!)
-           for (_, calendar) in source.calendars(for: .event).enumerated(){
-             print("  \(calendar.title)", calendar.value(forKey: "displayOrder")!)
-           }
+            print(source.title, source.value(forKey: "displayOrder")!)
+            for (_, calendar) in source.calendars(for: .event).enumerated(){
+                print("  \(calendar.title)", calendar.value(forKey: "displayOrder")!)
+            }
         }
     }
 }
